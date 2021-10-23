9f518bdd1d7b9ab6806cb545e9a775264549a4c5
Thomas Zhang <tzhangfox@gmail.com>
2021-10-23 16:33:40 -0700

    checkpoint 1

--- MinecraftAlert/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ MinecraftAlert/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace MinecraftAlert {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MinecraftAlert;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MinecraftAlert;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
